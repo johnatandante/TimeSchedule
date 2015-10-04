266af5fc1f063a9a5a641e5d2d21495e965affb9
Dante Del Favero <dante.delfavero@gmail.com>
2015-10-04 19:51:17 +0200

    Migrato in Aspnet

---
+++ Startup.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace TimeSchedule
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // to use static files
+            // https://wipdeveloper.com/2015/04/02/asp-net-5-and-static-files/
+            app.UseStaticFiles();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
