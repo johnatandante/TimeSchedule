0edeb0b548c8fd200db1771faabb7c432ba13dbc
Dante Del Favero <dante.delfavero@gmail.com>
2015-10-14 07:42:15 +0200

    Updgrade from Allianz. TdDo: fix current plan page

---
+++ wwwroot/common/utils.js
@@ -0,0 +1,19 @@
+﻿var u_timeToString = function (dateTime) {
+    var d = new Date(dateTime);
+    var minutes = d.getMinutes();
+    if (minutes < 10)
+        minutes = "0" + minutes;
+
+    return d.getHours() + ":" + minutes;
+
+};
+
+var u_reverse = function (itemA, itemB) {
+    if (itemA.time > itemB.time)
+        return -1;
+    else if (itemA.time < itemB.time)
+        return 1;
+    else
+        return 0;
+
+};
